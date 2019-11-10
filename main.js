26e2cd6433031140bb8d38b255186a13b31743d0
Peter Jones <peterjones@mac.com>
2019-11-10 16:06:06 -0500

    Changed from a 10 slice to eight slice pizza

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   'use strict';
   var x = 0;
-  console.log('Initial value: ' + x);
-  var num_slices = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    console.log('Number of slices: ' + num_slices.length);
+  var num_slices = ['0', '1', '2', '3', '4', '5', '6', '7'];
 });
